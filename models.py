a266d119c038bdd51a3ccddb14be24b0adbcf337
Dbar90 <georilla90@yahoo.com>
2021-12-10 21:13:38 -0500

    file structure/models/modules

---
+++ models.py
@@ -0,0 +1,41 @@
+from peewee import *
+import datetime
+
+
+DATABASE = SqliteDatabase('workouts.sqlite')
+
+class User(Model):
+    email = CharField()
+    password = CharField()
+    class Meta:
+        database = DATABASE
+
+
+
+class Workout(Model):
+    name = CharField()
+    # user = ForeignKeyField(User, backref='workouts')
+    created_at = DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        database = DATABASE
+
+
+class Lift(Model):
+    name = CharField()
+    start_weight = CharField()
+    current_weight = CharField()
+    sets = CharField()
+    reps = CharField()
+    persoanl_best = CharField()
+    notes = CharField()
+    workout = ForeignKeyField(Workout, backref='lifts')
+    created_at = DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Workout, Lift, User], safe=True)
+    print('Connected to DB.')
+    DATABASE.close()
